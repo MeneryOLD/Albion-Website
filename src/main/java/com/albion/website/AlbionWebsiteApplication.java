457b7cf86d19c39d305260f321985ed8685788ce
Menery <matvey.0711post@gmail.com>
2026-04-29 21:34:02 +0200

    The project was created

---
+++ src/main/java/com/albion/website/AlbionWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.albion.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbionWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbionWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albion/website/AlbionWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albion.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbionWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
